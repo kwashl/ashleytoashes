78aef80480ab853a7872a4405e0dc3b546fa108e
Ashley Kwon <ashleykwon@Ashleys-MacBook-Pro.local>
2024-08-16 02:15:33 -0700

    Updates

---
+++ precache-manifest.c977648d43aeff3a74d899e27eda5281.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8446b94b4a3e4c8a642a058efcd256",
+    "url": "/ashleytoashes/index.html"
+  },
+  {
+    "revision": "587647d818b380c259b3",
+    "url": "/ashleytoashes/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "19b653926d9b087a2e07",
+    "url": "/ashleytoashes/static/js/2.5ff95bdd.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ashleytoashes/static/js/2.5ff95bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/ashleytoashes/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "587647d818b380c259b3",
+    "url": "/ashleytoashes/static/js/main.bb65c50c.chunk.js"
+  },
+  {
+    "revision": "a02e8006b6ec2b68795a",
+    "url": "/ashleytoashes/static/js/runtime-main.a5de020f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ashleytoashes/static/media/logo.06e73328.svg"
+  }
+]);
